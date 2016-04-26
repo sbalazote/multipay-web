7d4666b832516a1791c67504bca13404702cb9dd
sbalazote <sbalazote@gmail.com>
2016-04-26 19:00:13 -0300

    customer y payment controllers

---
+++ Multipay/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Http;
+using System.Collections;
+using mercadopago;
+
+namespace Multipay.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        [HttpGet]
+        [Route("api/getCustomerCards")]
+        public string GetCustomerCards(string customerId)
+        {
+            MP mp = new MP(ConfigurationManager.AppSettings["MPAccessToken"]);
+
+            Hashtable cards = mp.get ("/v1/customers/"+customerId+"/cards", true);
+
+            Console.WriteLine (cards["response"].ToString());
+
+            return cards["response"].ToString();
+        }
+
+        [HttpGet]
+        [Route("api/getCustomer")]
+        public string GetCustomer(string customerId)
+        {
+            MP mp = new MP(ConfigurationManager.AppSettings["MPAccessToken"]);
+
+            Hashtable customer = mp.get("/v1/customers/"+customerId, true);
+
+            Console.WriteLine(customer["response"].ToString());
+
+            return customer["response"].ToString();
+        }
+    }
+}
