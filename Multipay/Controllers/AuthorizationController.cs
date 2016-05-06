594cbbab203e73e3a499d82a4702823f7587606d
sbalazote <sbalazote@gmail.com>
2016-05-06 14:56:56 -0300

    authorizationcontroller

--- Multipay/Controllers/AuthorizationController.cs
+++ Multipay/Controllers/AuthorizationController.cs
@@ -1,13 +1,12 @@
-﻿using log4net;
+﻿using System.Configuration;
+using log4net;
 using Model.Entities;
 using Multipay.DTOs;
-using Newtonsoft.Json;
 using System;
-using System.IO;
 using System.Net;
 using System.Reflection;
-using System.Text;
 using System.Web.Http;
+using RestSharp;
 using Services;
 
 namespace Multipay.Controllers
@@ -17,60 +16,50 @@ namespace Multipay.Controllers
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         private UserService UserService = new UserService();
-        
-        public string GetAuthorizationCode(string code, string email) {
+
+        [HttpGet]
+        [Route("api/authorization")]
+        public bool GetAuthorizationCode(string code, string email)
+        {
 
             Log.Debug("AuthorizationController.cs" + "   code: " + code + "email: " + email);
 
             // obtengo el usuario por el mail
             var user = (Seller)UserService.GetByEmail(email);
-            
-            string GrantType = "authorization_code";
-            string ClientId = "3108634673635661";
-            string ClientSecret = "4qCgUNhtRA3BnMJlAOjoXFZLT2EobUWJ";
-            string Code = code;
-            string RedirectURI = "http://multipay.ddns.net:8080/api/authorization";
-            
-            string postString = string.Format("grant_type={0}&client_id={1}&client_secret={2}&code={3}&redirect_uri={4}", GrantType, ClientId, ClientSecret, Code, RedirectURI);
-            
-            HttpWebRequest request = WebRequest.Create("https://api.mercadolibre.com/oauth/token") as HttpWebRequest;
-            request.Method = "POST";
-            request.ContentLength = postString.Length;
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.Accept = "application/json";
 
-            // Get the request stream.
-            Stream dataStream = request.GetRequestStream();
-            // Write the data to the request stream.
-            byte[] byteArray = Encoding.UTF8.GetBytes(postString);
-            dataStream.Write(byteArray, 0, byteArray.Length);
-            // Close the Stream object.
-            dataStream.Close();
-            // Get the response.
-            WebResponse response = request.GetResponse ();
-            // Display the status.
-            Console.WriteLine (((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.
-            dataStream = response.GetResponseStream ();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader (dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd ();
-            // Display the content.
-            Console.WriteLine (responseFromServer);
-            // Clean up the streams.
-            reader.Close ();
-            dataStream.Close ();
-            response.Close ();
+            var client = new RestClient("https://api.mercadolibre.com");
 
-            //TODO guardar los datos de tokens en el usuario.
-            var AuthToken = JsonConvert.DeserializeObject<AuthorizationTokenDTO>(responseFromServer);
-            user.Token.AccessToken = AuthToken.AccessToken;
-            user.Token.Expiration = AuthToken.ExpiresIn;
-            user.Token.RefreshToken = AuthToken.RefreshToken;
-            UserService.Save(user);
+            var request = new RestRequest("/oauth/token", Method.POST);
+            request.AddParameter("client_id", ConfigurationManager.AppSettings["AppId"]);
+            request.AddParameter("client_secret", ConfigurationManager.AppSettings["SecretKey"]);
+            request.AddParameter("grant_type", "authorization_code");
+            request.AddParameter("code", code);
+            request.AddParameter("redirect_uri", "https://faedf14b.ngrok.io/api/authorization?email=" + email);
 
-            return AuthToken.AccessToken;
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            var response = (RestResponse<AuthorizationTokenDTO>)client.Execute<AuthorizationTokenDTO>(request);
+
+            if (response.ResponseStatus == ResponseStatus.Completed && response.StatusCode == HttpStatusCode.OK)
+            {
+                var authorizationTokenDto = response.Data;
+
+                var token = new Token
+                {
+                    AccessToken = authorizationTokenDto.AccessToken,
+                    Expiration = authorizationTokenDto.ExpiresIn,
+                    RefreshToken = authorizationTokenDto.RefreshToken,
+                    RequestedTime = DateTime.Now,
+                    Scope = authorizationTokenDto.Scope,
+                    Type = authorizationTokenDto.TokenType
+                };
+                user.Token = token;
+                user.MPSellerUserId = authorizationTokenDto.UserId;
+                UserService.Save(user);
+                return true;
+            }
+            return false;
         }
-    } 
+    }
 }
