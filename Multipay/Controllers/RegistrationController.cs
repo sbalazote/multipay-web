82e9d4ef67919be520fde8f62a54b545d9bbc1b8
sbalazote <sbalazote@gmail.com>
2016-05-10 15:49:11 -0300

    Registration Controller y DAO

---
+++ Multipay/Controllers/RegistrationController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Http;
+using mercadopago;
+using Microsoft.Ajax.Utilities;
+using Model.Entities;
+using Multipay.DTOs;
+using Services;
+
+
+namespace Multipay.Controllers
+{
+    public class RegistrationController : ApiController
+    {
+        private UserService UserService = new UserService();
+
+        [HttpPost]
+        [Route("api/attemptNativeRegistration")]
+        public LoginResponseDTO AttemptNativeRegistration(RegistrationRequestDTO registrationRequest)
+        {
+            var loginValid = false;
+            var userId = -1;
+            var message = "";
+            var userName = "";
+            var userEmail = "";
+            var password = registrationRequest.Password;
+
+            var exists = UserService.Exists(registrationRequest.Email, registrationRequest.IsSeller);
+            if (!exists)
+            {
+
+                if (registrationRequest.IsSeller)
+                {
+                    var seller = new Seller
+                    {
+                        Email = registrationRequest.Email,
+                        Name = registrationRequest.Name,
+                        Date = DateTime.Now,
+                        Password = password,
+                        Active = true,
+                        Device = new Device
+                        {
+                            MobileId = registrationRequest.MobileId,
+                            RegistrationId = registrationRequest.RegistrationId
+                        },
+                    };
+                    UserService.Save(seller);
+                }
+                else
+                {
+                    var mp = new MP(ConfigurationManager.AppSettings["MPAccessToken"]);
+                    mp.sandboxMode(true);
+
+                    // Me fijo si ya existia ese Customer para ese email.
+                    var filters = new Dictionary<String, String> { { "email", registrationRequest.Email } };
+                    var customerSearch = mp.get("/v1/customers/search", filters);
+                    var customerSearchResponse = (Hashtable)customerSearch["response"];
+                    var results = (ArrayList)customerSearchResponse["results"];
+                    Hashtable resultsHashtable;
+                    string customerId;
+                    // No existe el Customer para ese mail.
+                    if (results.Count == 0)
+                    {
+                        var customerSaved = mp.post("/v1/customers", "{\"email\": \"" + registrationRequest.Email + "\"}");
+                        customerSearchResponse = (Hashtable) customerSaved["response"];
+                        resultsHashtable = (Hashtable) customerSearchResponse["results"];
+                        customerId = (string) resultsHashtable["id"];
+                    }
+                    // Existe el Customer para ese mail.
+                    else
+                    {
+                        resultsHashtable = (Hashtable)results[0];
+                        customerId = (string)resultsHashtable["id"];
+                        if (customerId.IsNullOrWhiteSpace())
+                        {
+                            var customerSaved = mp.post("/v1/customers", "{\"email\": \"" + registrationRequest.Email + "\"}");
+                            customerSearchResponse = (Hashtable)customerSaved["response"];
+                            results = (ArrayList)customerSearchResponse["results"];
+                            resultsHashtable = (Hashtable)results[0];
+                            customerId = (string)resultsHashtable["id"];
+                        }
+                    }
+                    
+                    var identification = new Identification
+                    {
+                        Type = registrationRequest.IdentificationType,
+                        Number = registrationRequest.IdentificationNumber
+                    };
+                    var address = new Address
+                    {
+                        Name = registrationRequest.Name,
+                        Number = registrationRequest.AddressNumber,
+                        ZipCode = registrationRequest.AddressZipCode
+                    };
+                    var phone = new Phone
+                    {
+                        AreaCode = registrationRequest.PhoneAreaCode,
+                        Number = registrationRequest.PhoneNumber
+                    };
+                    var buyer = new Buyer
+                    {
+                        Email = registrationRequest.Email,
+                        Name = registrationRequest.Name,
+                        Date = DateTime.Now,
+                        Password = registrationRequest.Password,
+                        Active = true,
+                        Device = new Device
+                        {
+                            MobileId = registrationRequest.MobileId,
+                            RegistrationId = registrationRequest.RegistrationId
+                        },
+                        MPCustomerId = (string)customerId,
+                        LastName = registrationRequest.LastName,
+                        Identification = identification,
+                        Address = address,
+                        Phone = phone
+                    };
+                    UserService.Save(buyer);
+                }
+                loginValid = true;
+                userName = registrationRequest.Name;
+                userEmail = registrationRequest.Email;
+
+            }
+            else
+            {
+                message = "Error - Usuario existente.";
+            }
+
+            var loginResponse = new LoginResponseDTO
+            {
+                Valid = loginValid,
+                Message = message,
+                UserId = userId,
+                UserName = userName,
+                UserEmail = userEmail
+            };
+
+            return loginResponse;
+        }
+    }
+}
---
+++ Multipay/DTOs/RegistrationRequestDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Multipay.DTOs
+{
+    public class RegistrationRequestDTO
+    {
+        [Required]
+        public string MobileId { get; set; }
+        [Required]
+        public string RegistrationId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string IdentificationType { get; set; }
+        public string IdentificationNumber { get; set; }
+        public string AddressName { get; set; }
+        public int AddressNumber { get; set; }
+        public string AddressZipCode { get; set; }
+        public int PhoneAreaCode { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required]
+        public bool IsSeller { get; set; }
+    }
+}
